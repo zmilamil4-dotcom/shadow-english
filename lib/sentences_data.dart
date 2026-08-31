da1678b98349035d39b15ca14de1863561a22d21
zmilamil4-dotcom <zmilamil4@gmail.com>
2026-08-31 10:37:24 +0100

    Create sentences_data.dart

---
+++ lib/sentences_data.dart
@@ -0,0 +1,9 @@
+import 'sentence_model.dart';
+
+const List<SentenceModel> sentencesData = [
+  SentenceModel(id: 1, text: 'Hello, how are you?'),
+  SentenceModel(id: 2, text: 'My name is...'),
+  SentenceModel(id: 3, text: 'I like learning English.'),
+  SentenceModel(id: 4, text: 'What are you doing?'),
+  SentenceModel(id: 5, text: 'Have a nice day.'),
+];
