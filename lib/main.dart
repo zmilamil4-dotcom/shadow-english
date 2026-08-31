ca74388e0f4a68b0fe49af24bb5cbae91c58c5b3
zmilamil4-dotcom <zmilamil4@gmail.com>
2026-08-31 09:54:08 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/splash_screen.dart';
+import 'state/app_state_provider.dart';
+import 'state/settings_provider.dart';
+import 'utils/theme.dart';
 
 void main() {
-  runApp(const ShadowEnglishApp());
+  runApp(const EchoSpeakApp());
 }
 
-class ShadowEnglishApp extends StatelessWidget {
-  const ShadowEnglishApp({super.key});
+class EchoSpeakApp extends StatelessWidget {
+  const EchoSpeakApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shadow English',
-      theme: ThemeData(primarySwatch: Colors.indigo, useMaterial3: true),
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Shadow English\nStage 0 OK ✅',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
+        ChangeNotifierProvider(create: (_) => AppStateProvider()),
+      ],
+      child: MaterialApp(
+        title: 'EchoSpeak',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        home: const SplashScreen(),
       ),
     );
   }
