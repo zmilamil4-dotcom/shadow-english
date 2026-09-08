9b9a0bae83bb4210be4b12ad3a3ce6571d18f9e7
zmilamil4-dotcom <zmilamil4@gmail.com>
2026-09-08 15:48:30 +0100

    Create glow_button.dart

---
+++ lib/widgets/lib/widgets/glow_button.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import '../app_theme.dart';
+
+class GlowButton extends StatefulWidget {
+  final String label;
+  final IconData? icon;
+  final Gradient gradient;
+  final VoidCallback onTap;
+  final double height;
+
+  const GlowButton({
+    super.key,
+    required this.label,
+    required this.onTap,
+    this.icon,
+    this.gradient = AppTheme.primaryGradient,
+    this.height = 56,
+  });
+
+  @override
+  State<GlowButton> createState() => _GlowButtonState();
+}
+
+class _GlowButtonState extends State<GlowButton> {
+  double _scale = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => setState(() => _scale = 0.97),
+      onTapUp: (_) => setState(() => _scale = 1.0),
+      onTapCancel: () => setState(() => _scale = 1.0),
+      onTap: widget.onTap,
+      child: AnimatedScale(
+        scale: _scale,
+        duration: const Duration(milliseconds: 100),
+        child: Container(
+          height: widget.height,
+          padding: const EdgeInsets.symmetric(horizontal: AppSpacing.lg),
+          decoration: BoxDecoration(
+            gradient: widget.gradient,
+            borderRadius: BorderRadius.circular(AppRadius.md),
+            boxShadow: [
+              BoxShadow(
+                color: AppTheme.accentPurple.withOpacity(0.35),
+                blurRadius: 18,
+                offset: const Offset(0, 8),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (widget.icon != null) ...[
+                Icon(widget.icon, color: Colors.white, size: 20),
+                const SizedBox(width: AppSpacing.sm),
+              ],
+              Text(widget.label, style: AppTextStyles.button),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GlowIconButton extends StatefulWidget {
+  final IconData icon;
+  final Gradient gradient;
+  final double size;
+  final VoidCallback onTap;
+
+  const GlowIconButton({
+    super.key,
+    required this.icon,
+    required this.onTap,
+    this.gradient = AppTheme.primaryGradient,
+    this.size = 56,
+  });
+
+  @override
+  State<GlowIconButton> createState() => _GlowIconButtonState();
+}
+
+class _GlowIconButtonState extends State<GlowIconButton> {
+  double _scale = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => setState(() => _scale = 0.94),
+      onTapUp: (_) => setState(() => _scale = 1.0),
+      onTapCancel: () => setState(() => _scale = 1.0),
+      onTap: widget.onTap,
+      child: AnimatedScale(
+        scale: _scale,
+        duration: const Duration(milliseconds: 100),
+        child: Container(
+          width: widget.size,
+          height: widget.size,
+          decoration: BoxDecoration(
+            gradient: widget.gradient,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: AppTheme.accentPurple.withOpacity(0.4),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: Icon(widget.icon, color: Colors.white, size: widget.size * 0.42),
+        ),
+      ),
+    );
+  }
+}
