3e69924eb415c4853fcc578afed11460ce246f0f
zmilamil4-dotcom <zmilamil4@gmail.com>
2026-09-12 13:18:20 +0100

    Update lesson_repository.dart

--- lib/lesson_repository.dart
+++ lib/lesson_repository.dart
@@ -1,9 +1,14 @@
 import 'lesson_model.dart';
 import 'lessons_data.dart';
 import 'data/a1_lessons.dart';
+import 'data/youtube_lessons.dart';
 
 class LessonRepository {
-  static List<LessonModel> _allLessons() => [...lessonsData, ...a1LessonsData];
+  static List<LessonModel> _allLessons() => [
+        ...lessonsData,
+        ...a1LessonsData,
+        ...youtubeLessonsData,
+      ];
 
   static List<LessonModel> getAll() => _allLessons();
 
